9fd1c755d02abdd180bac2e97145a36ac16bea03
andrecg16 <andrecg16@localhost>
2011-05-19 19:29:07 -0300

    test

--- LYAHV/LYAHV/src/lp2/projeto/CarregaEstabelecimentos.java
+++ LYAHV/LYAHV/src/lp2/projeto/CarregaEstabelecimentos.java
@@ -1,13 +1,21 @@
 package lp2.projeto;
 
 
-import java.util.*;
 
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Class CarregaEstabelecimentos
  */
+
 public class CarregaEstabelecimentos {
+	
+	private static List<String> lista = new ArrayList<String>();
 
   //
   // Fields
@@ -17,7 +25,21 @@ public class CarregaEstabelecimentos {
   //
   // Constructors
   //
-  public CarregaEstabelecimentos () { };
+  public CarregaEstabelecimentos () { ;
+  
+  String caminhoArquivo = "/home/yuri/pitaqueiro-lyahv/LYAHV/LYAHV/src/media/lista_estabelecimentos_projeto_lp2.csv";
+	try {
+		BufferedReader leitor = new BufferedReader(new FileReader(caminhoArquivo));
+		while(leitor.ready()){
+			System.out.println(leitor.readLine());
+			lista.add(leitor.readLine());
+		}
+		lista.toString();
+
+	} catch (IOException e) {
+	}
+  }
+
   
   //
   // Methods
@@ -35,9 +57,9 @@ public class CarregaEstabelecimentos {
   /**
    * @return       ListaDeEstabelecimentos
    */
-  public static ListaDeEstabelecimentos carregaEstabelecimentos(  )
-  {
-  }
+//  public static ListaDeEstabelecimentos carregaEstabelecimentos(  )
+//  {
+//  }
 
 
 }
