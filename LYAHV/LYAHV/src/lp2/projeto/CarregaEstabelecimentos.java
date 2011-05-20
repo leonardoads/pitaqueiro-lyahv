98695b7611a9f25039d0bfb83ede9572f35c5eb7
andrecg16 <andrecg16@localhost>
2011-05-20 16:21:12 -0300

    Ler Arquivos da entrada de dados

--- LYAHV/LYAHV/src/lp2/projeto/CarregaEstabelecimentos.java
+++ LYAHV/LYAHV/src/lp2/projeto/CarregaEstabelecimentos.java
@@ -14,49 +14,63 @@ import java.util.List;
  */
 
 public class CarregaEstabelecimentos {
-	
+
 	private static List<String> lista = new ArrayList<String>();
 
-  //
-  // Fieldss
-  //
+	//
+	// Fieldss
+	//
 
-  
-  //
-  // Constructors
-  //
-  public CarregaEstabelecimentos () { ;
-  
-  String caminhoArquivo = "/home/yuri/pitaqueiro-lyahv/LYAHV/LYAHV/src/media/lista_estabelecimentos_projeto_lp2.csv";
-	try {
-		BufferedReader leitor = new BufferedReader(new FileReader(caminhoArquivo));
-		while(leitor.ready()){
-			System.out.println(leitor.readLine());
-			lista.add(leitor.readLine());
+
+	//
+	// Constructors
+	//
+	public CarregaEstabelecimentos () {
+
+		String caminhoArquivo = "/home/yuri/pitaqueiro-lyahv/LYAHV/LYAHV/src/media/lista_estabelecimentos_projeto_lp2.csv";
+		try {
+			BufferedReader leitor = new BufferedReader(new FileReader(caminhoArquivo));
+			while(leitor.ready()){
+				lista.add(leitor.readLine());
+			}
+			lista.toString();
+
+		} catch (IOException e) {
 		}
-		lista.toString();
-
-	} catch (IOException e) {
 	}
-  }
+	
+	public void lerEstabelecimentos(){
+		for (int i = 0; i < lista.size(); i++) {
+			System.out.println(lista.get(i));
+		}
+	}
+	
+	public static void main(String[] args) {
+		CarregaEstabelecimentos file = new CarregaEstabelecimentos();
+		file.lerEstabelecimentos();
+		
+		
+	}
 
-  
-  //
-  // Methods
-  //
 
 
-  //
-  // Accessor methods
-  //
 
-  //
-  // Other methods
-  //
+//
+// Methods
+//
 
-  /**
-   * @return       ListaDeEstabelecimentos
-   */
+
+//
+// Accessor methods
+//
+
+//
+// Other methods
+//
+
+/**
+ * @return       ListaDeEstabelecimentos
+ */
 //  public static ListaDeEstabelecimentos carregaEstabelecimentos(  )
 //  {
 //  }
