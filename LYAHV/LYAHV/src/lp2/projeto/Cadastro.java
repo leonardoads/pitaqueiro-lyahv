e61f3f8a065bd7166cb33116f3420935d3b732b9
leonardoconstrutoresdapaz <leonardoconstrutoresdapaz@localhost>
2011-05-21 14:57:45 -0300

    Implementa??o da classe Cadastro

--- LYAHV/LYAHV/src/lp2/projeto/Cadastro.java
+++ LYAHV/LYAHV/src/lp2/projeto/Cadastro.java
@@ -1,5 +1,24 @@
 package lp2.projeto;
 
 public class Cadastro {
+	private Usuario usuario;
 
+	private Cadastro(String nome, ListaDePreferencias listaDePreferencia) {
+		usuario = new Usuario(nome, listaDePreferencia);
+	}
+	/**
+	 * 
+	 * @param nome a ser usado pelo usuario
+	 * @param listaDePreferencia 
+	 * @return
+	 * 			the cadastro if it successfully performed and otherwise returns null
+	 */
+	public static Cadastro cadastrar(String nome, ListaDePreferencias listaDePreferencia){
+		if(nome != null && !ListaUsuarios.getListaDeUsuarios().contains(nome)){
+			Cadastro cadastro = new Cadastro(nome, listaDePreferencia);
+			return cadastro;
+		}
+		return null;
+		
+	}
 }
