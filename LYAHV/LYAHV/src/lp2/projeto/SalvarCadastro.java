f1972d8eba01555905bb0e68f36ebc5fca5d51f6
leonardoconstrutoresdapaz <leonardoconstrutoresdapaz@localhost>
2011-05-21 23:40:28 -0300

    Cria??o e Implementa??o da Classe Salvar Cadastro

---
+++ LYAHV/LYAHV/src/lp2/projeto/SalvarCadastro.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package lp2.projeto;
+
+/**
+ * @author leonardo
+ *
+ */
+public class SalvarCadastro {
+public static boolean salvaCadastro(Cadastro cadastro){
+	return false;
+}
+
+}
