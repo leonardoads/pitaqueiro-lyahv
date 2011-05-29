544646237ee6a3d68824c7a1f16b9fd49f97bff5
leonardoconstrutoresdapaz <leonardoconstrutoresdapaz@localhost>
2011-05-29 01:41:36 -0300

    Corre??o no carregamento de arquivo

--- LYAHV/LYAHV/src/lp2/projeto/CarregarPerfis.java
+++ LYAHV/LYAHV/src/lp2/projeto/CarregarPerfis.java
@@ -4,9 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.*;
 
-import sun.management.OperatingSystemImpl;
 
 /**
  * Class CarregarPerfis
@@ -56,25 +54,33 @@ public class CarregarPerfis {
 		try {
 			file = new FileReader(caminhoArquivo + nomeArquivo);
 			bufferedRead = new BufferedReader(file);
+
+			// Prevents the program read the first line that contains the order
+			// of information in the file
 			bufferedRead.readLine();
+			
+			//Do read the file
 			while (bufferedRead.ready()) {
 				// System.out.println(bufferedRead.readLine());
 				line = bufferedRead.readLine().split(";");
+				
+				//avoids that the name be null
 				if (line[1].equals("")) {
 					nome = "Anonimo" + contadorDeAnonimos;
 					contadorDeAnonimos++;
 				} else {
 					nome = line[1];
 				}
+				
 				for (int i = 2; i < line.length; i++) {
+//					System.out.println(line[i]);
 					preferencias.adicionaPreferencia(line[i].split(":")[1]);
 				}
-				//Generate cadastro and save in ListaDeUsuarios
-				System.out.println(nome);
-				System.out.println(preferencias);
-				SalvarCadastro.salvaCadastro(
-						Cadastro.cadastrar(nome,preferencias)
-						);
+				
+				// Generate cadastro and save in ListaDeUsuarios
+				Cadastro c = new Cadastro(nome,
+						preferencias);
+				SalvarCadastro.salvaCadastro(c);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -83,9 +89,21 @@ public class CarregarPerfis {
 		}
 	}
 
-	public static void main(String[] args) throws IOException {
-		System.out.println(new File(caminhoArquivo
-				+ "opinioes-dos-usuarios-v1.csv").getCanonicalFile());
-		carregaArquivo("opinioes-dos-usuarios-v2.data");
-	}
+//	public static void main(String[] args) {
+//
+////		try {
+////			// System.out.println(new File(caminhoArquivo+
+////			// "opinioes-dos-usuarios-v2.data").getCanonicalFile());
+////			carregaArquivo("opinioes-dos-usuarios-v2.data");
+////			List<Usuario> a =  ListaUsuarios.getListaDeUsuarios();
+////			for (Iterator iterator = ListaUsuarios.getListaDeUsuarios().iterator(); iterator.hasNext();) {
+////				Usuario usuario = (Usuario) iterator.next();
+////				System.out.println(usuario);
+////			}
+////		} catch (Exception e) {
+////			// TODO Auto-generated catch block
+////			e.printStackTrace();
+////		}
+////		// carregaArquivo("opinioes-dos-usuarios-v2.data");
+//	}
 }
