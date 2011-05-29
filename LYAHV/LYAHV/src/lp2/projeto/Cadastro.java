838c578c298bb9af4fc03c95e4a86ff6360d0e2f
leonardoconstrutoresdapaz <leonardoconstrutoresdapaz@localhost>
2011-05-29 01:39:02 -0300

    Corre??o de metodos do codigo

--- LYAHV/LYAHV/src/lp2/projeto/Cadastro.java
+++ LYAHV/LYAHV/src/lp2/projeto/Cadastro.java
@@ -3,7 +3,7 @@ package lp2.projeto;
 public class Cadastro {
 	private Usuario usuario;
 
-	private Cadastro(String nome, ListaDePreferencias listaDePreferencia) {
+	Cadastro(String nome, ListaDePreferencias listaDePreferencia) {
 		usuario = new Usuario(nome, listaDePreferencia);
 	}
 	/**
@@ -13,13 +13,16 @@ public class Cadastro {
 	 * @return
 	 * 			the cadastro if it successfully performed and otherwise returns null
 	 */
-	public static Cadastro cadastrar(String nome, ListaDePreferencias listaDePreferencia){
-		if(nome != null && !ListaUsuarios.getListaDeUsuarios().contains(nome)){
-			System.out.println(nome+" "+ListaUsuarios.getListaDeUsuarios().contains(nome));
-			Cadastro cadastro = new Cadastro(nome, listaDePreferencia);
-			return cadastro;
-		}
-		return null;
-		
+//	public static Cadastro cadastrar(String nome, ListaDePreferencias listaDePreferencia){
+//		if(nome != null){
+//			System.out.println(nome+" "+ListaUsuarios.getListaDeUsuarios().contains(nome));
+//			Cadastro cadastro = new Cadastro(nome, listaDePreferencia);
+//			return cadastro;
+//		}
+//		return null;
+//		
+//	}
+	public Usuario getUsuario() {
+		return usuario;
 	}
 }
