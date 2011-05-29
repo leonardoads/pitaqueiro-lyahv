1173a6a4638f7455c0a6eabfaa2f50e466a2af31
leonardoconstrutoresdapaz <leonardoconstrutoresdapaz@localhost>
2011-05-29 01:38:03 -0300

    Melhoria e corre??o no metodo algoritimoPopularidade

--- LYAHV/LYAHV/src/lp2/projeto/Algoritmos.java
+++ LYAHV/LYAHV/src/lp2/projeto/Algoritmos.java
@@ -2,50 +2,68 @@ package lp2.projeto;
 
 import java.util.*;
 
-
 /**
  * Class Algoritimos
  */
 public class Algoritmos {
 
-  //
-  // Fields
-  //
+	//
+	// Fields
+	//
 
-  
-  //
-  // Constructors
-  //
-  public Algoritmos () { };
-  
-  //
-  // Methods
-  //
+	//
+	// Constructors
+	//
+	public Algoritmos() {
+	};
 
+	//
+	// Methods
+	//
 
-  //
-  // Accessor methods
-  //
+	//
+	// Accessor methods
+	//
 
-  //
-  // Other methods
-  //
+	//
+	// Other methods
+	//
 
-  /**
-   * @param        mapaEstabelecimentosOpinioes
-   */
-  public void popularidadeGlobal( MapaEstabelecimentosOpinioes mapaEstabelecimentosOpinioes )
-  {
-  }
+	/**
+	 * @param mapaEstabelecimentosOpinioes
+	 * @return
+	 */
+	public List<Estabelecimento> popularidadeGlobal() {
+		List<Estabelecimento> estabelecimentos = new ArrayList<Estabelecimento>();
+		Set popularidades = ListaDePopularidades.getMapaPopularidade().keySet();
 
+		Iterator it = popularidades.iterator();
+		int i = 0;
+		while (it.hasNext()) {
+			if (i == 0) {
 
-  /**
-   * @return       List<Estabelecimento>
-   * @param        mapaEstabelecimentosOpinioes
-   */
-  public List<Estabelecimento> personalizado( MapaEstabelecimentosOpinioes mapaEstabelecimentosOpinioes )
-  {
-  }
+			}
+			i++;
+		}
+		return estabelecimentos;
+	}
 
+	/**
+	 * @return List<Estabelecimento>
+	 * @param mapaEstabelecimentosOpinioes
+	 */
+	public List<Estabelecimento> personalizado(
+			MapaEstabelecimentosPreferencias mapaEstabelecimentosOpinioes) {
+		return null;
+	}
 
+	public static void main(String[] args) {
+		CarregaEstabelecimentos.carregaEstabelecimentos("lista_estabelecimentos_projeto_lp2.csv");
+		
+		ListaDePopularidades.gerarMapa(MapaEstabelecimentosPreferencias.transformaEmMapa());
+		
+		System.out.println(ListaDePopularidades.getMapaPopularidade().toString());
+		
+		
+	}
 }
