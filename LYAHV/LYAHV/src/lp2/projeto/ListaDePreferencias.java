4ca464f6306f2cb679069f3bed79b97e04fe7042
leonardoconstrutoresdapaz <leonardoconstrutoresdapaz@localhost>
2011-05-29 01:47:47 -0300

    Melhoramento no codigo

--- LYAHV/LYAHV/src/lp2/projeto/ListaDePreferencias.java
+++ LYAHV/LYAHV/src/lp2/projeto/ListaDePreferencias.java
@@ -34,10 +34,61 @@ public class ListaDePreferencias {
 	}
 
 	public void adicionaPreferencia(String nomePrefencia) {
+//		System.out.println(preferencias.length);
 		for (int i = 0; i < preferencias.length; i++) {
+			
 			if(preferencias[i].getNome().equals(nomePrefencia)){
 				listaDePreferencias.add(preferencias[i]);
 			}
 		}
+	
 	}
+	@Override
+	public String toString() {
+		return listaDePreferencias.toString();
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime
+				* result
+				+ ((listaDePreferencias == null) ? 0 : listaDePreferencias
+						.hashCode());
+		result = prime * result + Arrays.hashCode(preferencias);
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof ListaDePreferencias)) {
+			return false;
+		}
+		ListaDePreferencias other = (ListaDePreferencias) obj;
+		if (listaDePreferencias == null) {
+			if (other.listaDePreferencias != null) {
+				return false;
+			}
+		} else if (!listaDePreferencias.equals(other.listaDePreferencias)) {
+			return false;
+		}
+		if (!Arrays.equals(preferencias, other.preferencias)) {
+			return false;
+		}
+		return true;
+	}
+
 }
